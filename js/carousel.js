ce000023ca032b83a08c9b2911dc947d24c4f5ba
young924 <ahenaka9@gmail.com>
2020-08-18 13:51:12 +0900

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,30 @@
-const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelectorAll(".carousel-slide img");
+window.onload = function () {
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const carouselImages = document.querySelectorAll(".carousel-slide img");
 
-// buttons
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+  // buttons
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
 
-// counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+  // counter
+  let counter = 1;
+  const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+  // button listeners
+  nextBtn.addEventListener("click", () => {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  });
+
+  prevBtn.addEventListener("click", () => {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  });
+};
 
 /*
 const galleryContainer = document.querySelector(".gallery-container");
